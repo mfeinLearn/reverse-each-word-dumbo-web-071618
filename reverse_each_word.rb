983de29b3b62ac30c464dc1f2c977bc0ad50cc0d
Malcome Fein <mfein90@gmail.com>
2018-07-12 17:13:54 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,3 +3,9 @@ def reverse_each_word(str)
   new_arr = arr.collect {|arr| arr.reverse}
   new_arr.join(" ")
 end
+
+def reverse_each_word(str)
+  arr = str.split(" ")
+  new_arr = arr.each {|arr| arr.reverse}
+  new_arr.join(" ")
+end
