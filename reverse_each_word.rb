c0b8aef20c02eb19dd5a137ec8e157b4b972ce59
Malcome Fein <mfein90@gmail.com>
2018-08-02 17:10:09 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 def reverse_each_word(str)
   arr = str.split(" ")
   new_arr = arr.collect {|arr| arr.reverse}
@@ -8,4 +9,28 @@ def reverse_each_word(str)
   arr = str.split(" ")
   new_arr = arr.each {|arr| arr.reverse}
   new_arr.join(" ")
+=======
+# def reverse_each_word(sentence)
+#   sentence.split("").reverse
+# end
+
+# def reverse_each_word(str)
+#   arr = str.split /\b/
+#   new_arr = arr.collect {|a| a.reverse}
+#   new_arr.join
+# end
+
+
+
+
+
+
+
+
+
+def reverse_each_word(str)
+  arr = str.split /\b/
+  new_arr = arr.collect {|a| a.reverse}
+  new_arr.join
+>>>>>>> 5477be658ddd5aab8e828470ca72d3b378d006d8
 end
